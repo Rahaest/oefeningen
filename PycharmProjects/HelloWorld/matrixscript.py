1e688c307e3c49590fc2dbe7f637bbb6c2697545
Raf Haest <haest.raf@gmail.com>
2022-08-12 11:43:58 +0200

    Adding Python exercise

---
+++ PycharmProjects/HelloWorld/matrixscript.py
@@ -0,0 +1,24 @@
+import re
+
+# Input rows & columns
+script_input = input().split()
+n = int(script_input[0])
+m = int(script_input[1])
+
+matrix = []
+# Input of data
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+# Zip combines the lists, * is used to unpack arguments from the list
+matrix = list(zip(*matrix))
+
+message = ""
+
+for word in matrix:
+    for char in word:
+        message += char
+# ?<= is a lookbehind , ?= a lookahead, meaning that before and after should be an alphanumeric character (\w).
+# \W+ will match any character that's not alphanumeric and replaces them with space.
+print(re.sub(r"(?<=\w)(\W+)(?=\w)", ' ', message))
